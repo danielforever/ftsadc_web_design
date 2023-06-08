abc855294b68028417af81c857f454ecc32815fc
danielforever <daniel87011354@gmail.com>
2023-06-08 12:46:18 -0400

    connecting mogoose db with middleware

--- server/index.js
+++ server/index.js
@@ -2,15 +2,22 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require('path');
-const { logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 let mongoose = require('mongoose');
 let cors = require('cors');
 const cookieParser = require('cookie-parser');
 const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn')
 let bodyParser = require('body-parser');
 const userRoute = require('../server/routes/blog');
-const dbURL = 'mongodb+srv://testuser:12345@cluster0.qrz4436.mongodb.net/test';
+//const dbURL = 'mongodb+srv://testuser:12345@cluster0.qrz4436.mongodb.net/test';
+const PORT = process.env.PORT || 3500;
 
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger);
 
@@ -18,12 +25,24 @@ app.use(cors(corsOptions));
 
 app.use(express.json())
 
-app.use(cookieParser)
+app.use(cookieParser())
 
+app.use('/', express.static(path.join(__dirname, 'public')))
 
+app.use('/', require('./routes/root'))
 
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+      res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+      res.json({ message: '404 Not Found' })
+  } else {
+      res.type('txt').send('404 Not Found')
+  }
+})
 
-mongoose
+/* mongoose
   .connect(dbURL)
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
@@ -35,23 +54,35 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
-}));
+})); */
 
 
 
 
-app.use('/users', userRoute)
-const port = process.env.PORT || 3500;
+/* app.use('/users', userRoute) */
 
-const server = app.listen(port, () => {
-    console.log('Connected to port ' + port)
-})
+
+/* const server = app.listen(PORT, () => {
+    console.log('Connected to port ' + PORT)
+}) */
 // Error Handling
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     next(createError(404));
 });
 app.use(function (err, req, res, next) {
     console.error(err.message);
     if (!err.statusCode) err.statusCode = 500;
     res.status(err.statusCode).send(err.message);
-});
+}); */
+
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB')
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+  console.log(err)
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
