e86e68f7d7c8392eb2761b652fafe7f4a24f65c6
danielforever <daniel87011354@gmail.com>
2023-06-10 23:57:17 -0400

    update comment in server

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ console.log(process.env.NODE_ENV);
 
 connectDB();
 
-// show logger commands in ./logs
+// show logger actions in ./logs/reqLog.log
 app.use(logger);
 
 // CORS for Nodejs and React to interact
