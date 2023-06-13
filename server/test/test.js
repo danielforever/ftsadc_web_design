98bdbd9e49a82f66e1e41da337c55d07e959f1a5
Tuna0128 <tn0128wayne@gmail.com>
2023-06-13 01:04:15 -0400

    Update test.js
    Added tests for GET /users and POST /users

--- server/test/test.js
+++ server/test/test.js
@@ -1,15 +1,51 @@
-var request = require('supertest');
-var app = require('../index.js');
+let request = require('supertest');
+let app = require('../index.js');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+chai.use(chaiHttp);
+
 describe('GET /', function () {
     it('respond with hello world', function (done) {
-        //navigate to root and check the the response is "hello world"
+        // navigate to root and check the the response is "hello world"
         request(app).get('/').expect('hello world', done);
     });
 });
 
-// describe('GET /users', function () {
-//     it('respond with hello world', function (done) {
-//         //navigate to root and check the the response is "hello world"
-//         request(app).get('/users').expect('hello world', done);
-//     });
-// });
+describe('GET /users', function () {
+    it('it should GET all the users', function (done) {
+        chai.request(app)
+            .get('/users')
+            .end((err, res) => {
+                res.should.have.status(200); // Response code
+                res.body.should.be.a('array'); // Data type
+                res.body.length.should.be.eql(3); // The number of users
+                done();
+            });
+    });
+});
+
+
+/*
+* Test the /POST route
+*/
+describe('POST /users', () => {
+    it('it should not POST a user without pages field', (done) => {
+        let user = {
+            username: "testuser",
+            email: "testemail@abc.com",
+        }
+      chai.request(app)
+          .post('/users')
+          .send(user)
+          .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message').eql('All fields are required');
+            done();
+          });
+    });
+
+});
