2eca21cd8b16931fa1becef762d425b7d1e7b272
danielforever <daniel87011354@gmail.com>
2023-06-15 16:16:56 -0400

    Add poster query update

--- client/src/features/posters/PostersList.js
+++ client/src/features/posters/PostersList.js
@@ -8,7 +8,11 @@ const PostersList = () => {
         isSuccess,
         isError,
         error
-    } = useGetPostersQuery()
+    } = useGetPostersQuery(undefined,  {
+        pollingInterval: 15000,
+        refetchOnFocus: true,
+        refetchOnMountOrArgChange: true
+    })
 
     let content
 
