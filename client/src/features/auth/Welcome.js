43c09c302402c8403b0c6eed2d6683ab4e279fb6
danielforever <daniel87011354@gmail.com>
2023-06-15 22:42:00 -0400

    Add edit and create new poster form

--- client/src/features/auth/Welcome.js
+++ client/src/features/auth/Welcome.js
@@ -12,9 +12,9 @@ const Welcome = () => {
 
             <h1>Welcome!</h1>
 
-            <p><Link to="/dash/notes">View techNotes</Link></p>
+            <p><Link to="/dash/posters">View Created Poster</Link></p>
 
-            <p><Link to="/dash/notes/new">Add New techNote</Link></p>
+            <p><Link to="/dash/Posters/new">Add New Poster</Link></p>
 
             <p><Link to="/dash/users">View User Settings</Link></p>
 
--- client/src/features/posters/EditPosterForm.js
+++ client/src/features/posters/EditPosterForm.js
@@ -1,11 +1,160 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { useUpdatePosterMutation, useDeletePosterMutation } from "./postersApiSlice"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons"
 
-const EditPosterForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const EditPosterForm = ({ poster, users }) => {
+    const [updatePoster, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useUpdatePosterMutation()
+
+    const [deletePoster, {
+        isSuccess: isDelSuccess,
+        isError: isDelError,
+        error: delerror
+    }] = useDeletePosterMutation()
+
+    const navigate = useNavigate()
+
+    const [title, setTitle] = useState(poster.title)
+    const [text, setText] = useState(poster.text)
+    const [completed, setCompleted] = useState(poster.completed)
+    const [userId, setUserId] = useState(poster.user)
+
+    useEffect(() => {
+
+        if (isSuccess || isDelSuccess) {
+            setTitle('')
+            setText('')
+            setUserId('')
+            navigate('/dash/posters')
+        }
+
+    }, [isSuccess, isDelSuccess, navigate])
+
+    const onTitleChanged = e => setTitle(e.target.value)
+    const onTextChanged = e => setText(e.target.value)
+    const onCompletedChanged = e => setCompleted(prev => !prev)
+    const onUserIdChanged = e => setUserId(e.target.value)
+
+    const canSave = [title, text, userId].every(Boolean) && !isLoading
+
+    const onSavePosterClicked = async (e) => {
+        if (canSave) {
+            await updatePoster({ id: poster.id, user: userId, title, text, completed })
+        }
+    }
+
+    const onDeletePosterClicked = async () => {
+        await deletePoster({ id: poster.id })
+    }
+
+    const created = new Date(poster.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+    const updated = new Date(poster.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+
+    const options = users.map(user => {
+        return (
+            <option
+                key={user.id}
+                value={user.id}
+
+            > {user.username}</option >
+        )
+    })
+
+    const errClass = (isError || isDelError) ? "errmsg" : "offscreen"
+    const validTitleClass = !title ? "form__input--incomplete" : ''
+    const validTextClass = !text ? "form__input--incomplete" : ''
+
+    const errContent = (error?.data?.message || delerror?.data?.message) ?? ''
+
+    const content = (
+        <>
+            <p className={errClass}>{errContent}</p>
+
+            <form className="form" onSubmit={e => e.preventDefault()}>
+                <div className="form__title-row">
+                    <h2>Edit Poster #{poster.ticket}</h2>
+                    <div className="form__action-buttons">
+                        <button
+                            className="icon-button"
+                            title="Save"
+                            onClick={onSavePosterClicked}
+                            disabled={!canSave}
+                        >
+                            <FontAwesomeIcon icon={faSave} />
+                        </button>
+                        <button
+                            className="icon-button"
+                            title="Delete"
+                            onClick={onDeletePosterClicked}
+                        >
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                    </div>
+                </div>
+                <label className="form__label" htmlFor="poster-title">
+                    Title:</label>
+                <input
+                    className={`form__input ${validTitleClass}`}
+                    id="poster-title"
+                    name="title"
+                    type="text"
+                    autoComplete="off"
+                    value={title}
+                    onChange={onTitleChanged}
+                />
+
+                <label className="form__label" htmlFor="poster-text">
+                    Text:</label>
+                <textarea
+                    className={`form__input form__input--text ${validTextClass}`}
+                    id="poster-text"
+                    name="text"
+                    value={text}
+                    onChange={onTextChanged}
+                />
+                <div className="form__row">
+                    <div className="form__divider">
+                        <label className="form__label form__checkbox-container" htmlFor="poster-completed">
+                            WORK COMPLETE:
+                            <input
+                                className="form__checkbox"
+                                id="poster-completed"
+                                name="completed"
+                                type="checkbox"
+                                checked={completed}
+                                onChange={onCompletedChanged}
+                            />
+                        </label>
+
+                        <label className="form__label form__checkbox-container" htmlFor="poster-username">
+                            ASSIGNED TO:</label>
+                        <select
+                            id="poster-username"
+                            name="username"
+                            className="form__select"
+                            value={userId}
+                            onChange={onUserIdChanged}
+                        >
+                            {options}
+                        </select>
+                    </div>
+                    <div className="form__divider">
+                        <p className="form__created">Created:<br />{created}</p>
+                        <p className="form__updated">Updated:<br />{updated}</p>
+                    </div>
+                </div>
+            </form>
+        </>
+    )
+
+    return content
 }
+
 
 export default EditPosterForm
