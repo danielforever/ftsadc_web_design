45204de022412ca52569cbbf27b9808ac8eed87e
Tuna0128 <tn0128wayne@gmail.com>
2023-07-01 14:54:23 -0400

    Add component authorization (buggy)
    Need to fix the problem "TypeError: Cannot read properties of null (reading 'useContext')"

--- client/src/features/auth/RequireAuthComp.js
+++ client/src/features/auth/RequireAuthComp.js
@@ -1,17 +1,34 @@
-import { useLocation, Navigate, Outlet } from "react-router-dom"
-import useAuth from "../../hooks/useAuth"
+// import { useLocation, Navigate, Outlet } from "react-router-dom"
+// import useAuth from "../../hooks/useAuth"
+// import { ROLES } from '../../config/roles'
+import { Outlet, Link } from "react-router-dom"
+import { useEffect, useRef, useState } from 'react'
+import { useRefreshMutation } from "./authApiSlice"
+import usePersist from "../../hooks/usePersist"
+import { useSelector } from 'react-redux';
+import { selectCurrentToken } from "./authSlice"
 
-// To avoid anyone who bypass the authorized step by knowing the url
-const RequireAuthComp = ({ allowedRoles }) => {
-    const location = useLocation()
-    const { roles } = useAuth()
+//TODO:Fix "TypeError: Cannot read properties of null (reading 'useContext')"
+const RequireAuthComp = (WrappedComponent) => {
+    try {
+        const accessToken = useSelector(selectCurrentToken);
+        console.log("accessToken")
+        console.log(accessToken)
+        return (props) => {
+            if (typeof window !== 'undefined') {
+                //const router = useRouter();
+                if (false) {
+                    //router.replace('/login');
+                    return "null";
+                }
 
-    const content = (
-        roles.some(role => allowedRoles.includes(role))
-            ? <Outlet />
-            : <Navigate to="/login" state={{ from: location }} replace />
-    )
-
-    return content
-}
+                return <WrappedComponent {...props} />;
+            }
+            return null;
+        };
+    } catch (error) {
+        console.error(error);
+        return "null"
+    }
+};
 export default RequireAuthComp
