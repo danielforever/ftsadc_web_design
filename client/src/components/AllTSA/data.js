7fbf464e79ee678f8be982464a60e46773073dcd
Tuna0128 <tn0128wayne@gmail.com>
2023-07-11 14:43:14 -0400

    Changed to larger images

--- client/src/components/AllTSA/data.js
+++ client/src/components/AllTSA/data.js
@@ -1,7 +1,7 @@
 export const allstates = [
     {
       id: 1,
-      src: require("./images/dc-small.jpg"),
+      src: require("./images/dc-medium.jpg"),
       state: "Washington DC",
       ch_state: "華盛頓特區",
       description: "Washington DC is the capital of the United States, known for its iconic monuments, political power, and historical significance.",
@@ -9,7 +9,7 @@ export const allstates = [
     },
     {
       id: 2,
-      src: require("./images/md-small.jpg"),
+      src: require("./images/md-medium.jpg"),
       state: "Maryland",
       ch_state: "馬里蘭州",
       description: "Maryland is known for its delicious blue crabs and being home to the United States Naval Academy in Annapolis.",
@@ -17,7 +17,7 @@ export const allstates = [
     },
     {
       id: 3,
-      src: require("./images/va-small.jpg"),
+      src: require("./images/va-medium.jpg"),
       state: "Virginia",
       ch_state: "維吉尼亞州",
       description: "Virginia is a state where the past meets the present, with stunning natural landscapes, vibrant cities, and a history so rich you can almost taste it.",
@@ -25,7 +25,7 @@ export const allstates = [
     },
     {
       id: 4,
-      src: require("./images/wv-small.jpg"),
+      src: require("./images/wv-medium.jpg"),
       state: "West Virginia",
       ch_state: "西維吉尼亞州",
       description: "West Virginia, known as \"The Mountain State,\" features the iconic New River Gorge Bridge and a rich coal mining history.",
@@ -33,7 +33,7 @@ export const allstates = [
     },
     {
       id: 5,
-      src: require("./images/nc-small.jpg"),
+      src: require("./images/nc-medium.jpg"),
       state: "North Carolina",
       ch_state: "北卡羅來納州",
       description: "North Carolina, located on the southeastern coast of the United States, is known for its beautiful beaches, vibrant cities, and rich cultural heritage.",
@@ -41,7 +41,7 @@ export const allstates = [
     },
     {
       id: 6,
-      src: require("./images/sc-small.jpg"),
+      src: require("./images/sc-medium.jpg"),
       state: "South Carolina",
       ch_state: "南卡羅來納州",
       description: "South Carolina is known for its picturesque coastline, historic plantations, and is the birthplace of sweet tea, a popular and refreshing Southern beverage.",
@@ -49,7 +49,7 @@ export const allstates = [
     },
     {
       id: 7,
-      src: require("./images/ga-small.jpg"),
+      src: require("./images/ga-medium.jpg"),
       state: "Georgia",
       ch_state: "喬治亞州",
       description: "Georgia offers a mix of southern charm, vibrant cities like Atlanta, and is the birthplace of Coca-Cola, the iconic soft drink that originated in the city of Atlanta in 1886.",
@@ -57,7 +57,7 @@ export const allstates = [
     },
     {
       id: 8,
-      src: require("./images/fl-small.jpg"),
+      src: require("./images/fl-medium.jpg"),
       state: "Florida",
       ch_state: "佛羅里達州",
       description: "Florida is renowned for its sunny beaches, theme parks like Walt Disney World, and is the only state in the country that is bordered by both the Atlantic Ocean and the Gulf of Mexico.",
@@ -65,7 +65,7 @@ export const allstates = [
     },
     {
       id: 9,
-      src: require("./images/ky-small.jpg"),
+      src: require("./images/ky-medium.jpg"),
       state: "Kentucky",
       ch_state: "肯塔基州",
       description: "Kentucky is famous for the Kentucky Derby and being the birthplace of bluegrass music.",
@@ -73,7 +73,7 @@ export const allstates = [
     },
     {
       id: 10,
-      src: require("./images/tn-small.jpg"),
+      src: require("./images/tn-medium.jpg"),
       state: "Tennessee",
       ch_state: "田納西州",
       description: "Tennessee is home to the iconic city of Nashville, known as the Music City, and the Great Smoky Mountains National Park, the most visited national park in the country.",
@@ -81,7 +81,7 @@ export const allstates = [
     },
     {
       id: 11,
-      src: require("./images/de-small.jpg"),
+      src: require("./images/de-medium.jpg"),
       state: "Delaware",
       ch_state: "德拉瓦州",
       description: "Delaware, a small state on the East Coast of the United States, is renowned for its tax-free shopping and outlet centers that offer discounted prices on a variety of products.",
@@ -89,7 +89,7 @@ export const allstates = [
     },
     {
       id: 12,
-      src: require("./images/al-small.jpg"),
+      src: require("./images/al-medium.jpg"),
       state: "Alabama",
       ch_state: "阿拉巴馬州",
       description: "Alabama is famous for its rich civil rights history and being the birthplace of the iconic American sport, football, with the University of Alabama's Crimson Tide football team enjoying great success.",
