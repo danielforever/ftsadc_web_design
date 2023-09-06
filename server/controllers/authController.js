dbc5e2a04fd11f889675eb25dfd3a0a42208ccdc
mikeleegithub <131956795+mikeleegithub@users.noreply.github.com>
2023-09-06 18:29:21 -0400

    new comment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
-
+//test
 // @desc Login
 // @route POST /auth
 // @access Public
